9983a62888fa9fcdeea53cfdf94a119696754fc8
Windyboy <y765432100@hotmail.com>
2022-08-01 21:42:59 +0800

    修正啟動Bat

--- 启动服务端.bat
+++ 启动服务端.bat
@@ -1,8 +1,7 @@
 @echo off
 @title   CongMS(启动后可关闭）QQ327321366
 Color 0A
-set path=config\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%
 set CLASSPATH=.;lib\*;
 
-javaw -XX:-UseGCOverheadLimit -Xms512m -Xmx1024m -Xss128k -Dnet.sf.odinms.wzpath=wz gui.CongMS
+java -XX:-UseGCOverheadLimit -Xms512m -Xmx1024m -Dnet.sf.odinms.wzpath=wz gui.CongMS
 pause
